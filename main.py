f2ed26b80924243b3c11148c852741a70afa432e
Shubham Jadhao <shubhamjadhao1124@gmail.com>
2026-08-22 13:27:35 +0530

    add main.py file

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+print("-------- Expense Tracker --------")
+print("1. Add Expense.")
+print("2. View Expense.")
+print("3. Delete Expense.")
+print("4. Totoal Expanse.")
+print("5. Exit")
+
+choice = input("Enter your choice: ")
+
+while True:
+    if choice == "1":
+        pass
+    
