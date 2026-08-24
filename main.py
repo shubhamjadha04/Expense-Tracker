c8dc4f3a503d64881e0200038bac6d1e22aa4175
Shubham Jadhao <shubhamjadhao1124@gmail.com>
2026-08-24 22:08:05 +0530

    Update main.py
    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from database import conn, cursor
 
 
 # Registration function
-def Register_function():
+def register_user():
     name = input("Enter Your Name: ")
     email = input("Enter Your email: ")
     password = input("Enter Your Password: ")
@@ -14,11 +14,11 @@ def Register_function():
     conn.commit()
 
     print("Registration Successful!..")
-    track_expense()
+    expense_menu()
 
 
 # Login function
-def login_function():
+def login_user():
     email = input("Enter Your email: ")
     password = input("Enter Your password: ")
 
@@ -31,15 +31,17 @@ def login_function():
 
     if user:
         print("Login Successful!..")
-        track_expense()
+        expense_menu()
 
     else:
         print("Invalid email or password..")
     
 
-# The main expense tarctor function
 
-def track_expense():
+
+
+# The main expense tarctor menu
+def expense_menu():
     print("-------- Expense Tracker --------")
     print("1. Add Expense.")
     print("2. View Expense.")
@@ -51,7 +53,14 @@ def track_expense():
 
     while True:
         if choice == "1":
-            pass
+            Category = input("Enter the categery: ")
+            amount  = int(input("Enter the amount: "))
+            Description = input("Enter the Description: ")
+
+            query = """
+                        INSERT INTO 
+                        
+                            """
 
 
 
@@ -74,10 +83,9 @@ print("2. login: ")
 
 log_choice = input("Enter your choice: ")
 if log_choice == "1":
-    Register_function()
-
+    register_user()
 elif log_choice == "2":
-    login_function()
+    login_user()
 else:
     print("Invalid Option..")
 
